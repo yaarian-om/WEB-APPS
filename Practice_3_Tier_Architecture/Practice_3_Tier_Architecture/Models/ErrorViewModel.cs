067d15cb065740f994615d37b7dab0e8b47196a3
yaarian-om <dip.kumar020@gmail.com>
2023-05-26 12:12:01 +0600

    Updating Project

---
+++ Practice_3_Tier_Architecture/Practice_3_Tier_Architecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practice_3_Tier_Architecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
